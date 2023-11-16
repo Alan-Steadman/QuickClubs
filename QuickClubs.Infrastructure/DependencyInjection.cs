a531d7b80eff6fadf61b848c5e4abfc1b0f4025f
Alan Steadman <alan.steadman@gmail.com>
2023-11-16 23:05:24 +0000

    Added Infrastructure project and Persistence with scaffolding for EF Core and Sql Server

---
+++ QuickClubs.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuickClubs.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddPersistence(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddPersistence(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        var connectionString =
+            configuration.GetConnectionString("Database") ??
+                throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+            options.EnableDetailedErrors();
+            options.EnableSensitiveDataLogging();
+        });
+
+
+        services.AddScoped<IGuideRepository, GuideRepository>();
+        services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
+        services.AddScoped<IModelRepository, ModelRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+}
---
+++ QuickClubs.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickClubs.Domain.Abstractions;
+
+namespace QuickClubs.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
