54875df952decaf5bb67738be6c25f9370055c12
Alan Steadman <alan.steadman@gmail.com>
2023-11-25 23:56:13 +0000

    event handler sends an email when user registers

---
+++ src/QuickClubs.Application/Abstractions/Email/EmailMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace QuickClubs.Application.Abstractions.Email;
+
+public class EmailMessage : IEmailMessage
+{
+    public List<string> ToEmail { get; init; }
+    public string Subject { get; init; }
+    public string Body { get; init; }
+
+    public EmailMessage(List<string> toEmail, string subject, string body)
+    {
+        ToEmail = toEmail;
+        Subject = subject;
+        Body = body;
+    }
+}
---
+++ src/QuickClubs.Application/Authentication/Register/UserCreatedDomainEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using QuickClubs.Application.Abstractions.Email;
+using QuickClubs.Domain.Users.Events;
+using QuickClubs.Domain.Users.Repository;
+using System.Net.Mail;
+
+namespace QuickClubs.Application.Authentication.Register;
+
+internal sealed class UserCreatedDomainEventHandler : INotificationHandler<UserCreatedDomainEvent>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailService _emailService;
+
+    public UserCreatedDomainEventHandler(IUserRepository userRepository, IEmailService emailService)
+    {
+        _userRepository = userRepository;
+        _emailService = emailService;
+    }
+
+    public async Task Handle(UserCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(notification.UserId);
+        if (user is null)
+            return;
+
+        var message = new EmailMessage(
+            [user.Email.Value],
+            "Verification",
+            "Please verify your email address using code 123");
+
+        await _emailService.SendEmailAsync(message);
+
+    }
+}
