1cf4d20bdf3e23322cc35c9b6d5477c79ea5bc04
Alan Steadman <alan.steadman@gmail.com>
2023-12-01 15:56:07 +0000

    Parameterized MembershipErrors

--- src/QuickClubs.Domain/Memberships/Errors/MembershipErrors.cs
+++ src/QuickClubs.Domain/Memberships/Errors/MembershipErrors.cs
@@ -4,6 +4,6 @@ namespace QuickClubs.Domain.Memberships.Errors;
 
 public static class MembershipErrors
 {
-    public static Error NotFound => new ("Membership.NotFound", "No membership was found with that identifier");
+    public static Error NotFound(Guid id) => new ("Membership.NotFound", $"No membership was found with the id {id}");
     public static Error AlreadyApproved => new ("Membership.AlreadyApproved", "This membership is already approved");
 }
