ef5ca224c80da5446c6fade32e754e592bdd53bc
Alan Steadman <alan.steadman@gmail.com>
2023-12-17 20:57:34 +0000

    Attempt to create ClubEdit , however, not working because BlazorWebAppClient not set up correctly.  About to remove AdminUI project and recreate.

---
+++ src/QuickClubs.AdminUI/Common/Listeners/NotFoundListener.cs
@@ -0,0 +1,14 @@
+﻿namespace QuickClubs.AdminUI.Common.Listeners;
+
+public class NotFoundListener
+{
+    public Action OnNotFound { get; set; } = null!;
+
+    public void NotifyNotFound()
+    {
+        if (NotifyNotFound != null)
+        {
+            OnNotFound.Invoke();
+        }
+    }
+}
--- src/QuickClubs.AdminUI/Program.cs
+++ src/QuickClubs.AdminUI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
 using QuickClubs.AdminUI.Clubs;
+using QuickClubs.AdminUI.Common.Listeners;
 using QuickClubs.AdminUI.Common.Models;
 using QuickClubs.AdminUI.Components;
 
@@ -9,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddHttpContextAccessor(); // needed for the NotFoundListener on MainLayout.razor
+builder.Services.AddScoped<NotFoundListener>();
 
 builder.Services.AddOptions<ApiSettings>()
     .BindConfiguration(ApiSettings.SectionName)
