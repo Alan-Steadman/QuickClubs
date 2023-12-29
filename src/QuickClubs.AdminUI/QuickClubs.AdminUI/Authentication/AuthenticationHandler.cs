89fb202e0e7cac9e3bc4656be1a28f2c7062cf03
Alan Steadman <alan.steadman@gmail.com>
2023-12-29 23:10:34 +0000

    Added spacing to buttons on Clubs List

--- src/QuickClubs.AdminUI/QuickClubs.AdminUI/Authentication/AuthenticationHandler.cs
+++ src/QuickClubs.AdminUI/QuickClubs.AdminUI/Authentication/AuthenticationHandler.cs
@@ -16,6 +16,8 @@ public class AuthenticationHandler : DelegatingHandler
     {
         if (request.Headers.Authorization?.Scheme != "Bearer")
         {
+            //request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3OGE0Njc0MC1lYTU0LTQzZTAtOTRkNy0yOTgxMDYxNWZjNGMiLCJlbWFpbCI6ImhlbnJ5QGludGVybmV0LmNvbSIsImdpdmVuX25hbWUiOiJoZW5yeSIsImZhbWlseV9uYW1lIjoidHVkb3IiLCJqdGkiOiJkOGI2OWM0ZC00NDJjLTRkYzItOWU1ZC1jOWQyMTc4M2Y0YTAiLCJleHAiOjE3MDM4NDk0MTYsImlzcyI6InF1aWNrY2x1YnMiLCJhdWQiOiJxdWlja2NsdWJzIn0.4N1CPfgc8Uh0xY4zO2xJ0dXaEFAq2HnAWuLtv7aW0vQ");
+
             var savedTokenResult = await _sessionStorage.GetAsync<string>(StorageConstants.Local.AuthToken);
             var savedToken = savedTokenResult.Success ? savedTokenResult.Value : null;
 
--- src/QuickClubs.AdminUI/QuickClubs.AdminUI/Services/ClubService.cs
+++ src/QuickClubs.AdminUI/QuickClubs.AdminUI/Services/ClubService.cs
@@ -1,20 +1,35 @@
-﻿using QuickClubs.AdminUI.Extensions;
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using QuickClubs.Contracts.Constants.Storage;
+using System.Net.Http.Headers;
+
+using QuickClubs.AdminUI.Extensions;
 using QuickClubs.Contracts.Clubs;
+using System.Net.Http;
 using System.Net.Http.Json;
 
 namespace QuickClubs.AdminUI.Services;
 
 public class ClubService : IClubService
 {
+    private readonly ProtectedSessionStorage _sessionStorage;
     private readonly HttpClient _httpClient;
 
-    public ClubService(HttpClient httpClient)
+    public ClubService(ProtectedSessionStorage sessionStorage, HttpClient httpClient)
     {
+        _sessionStorage = sessionStorage;
         _httpClient = httpClient;
     }
 
     public async Task<IEnumerable<ClubResponse>?> GetAllClubs()
     {
+        //var savedTokenResult = await _sessionStorage.GetAsync<string>(StorageConstants.Local.AuthToken);
+        //var savedToken = savedTokenResult.Success ? savedTokenResult.Value : null;
+
+        //if (!string.IsNullOrWhiteSpace(savedToken))
+        //{
+        //    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", savedToken);
+        //}
+
         var content = await _httpClient.GetFromJsonAsync<IEnumerable<ClubResponse>>("");
         return content;
     }
