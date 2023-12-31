b6835f46b66b8a0d2a236aae3281ba32cdfd2a21
Alan Steadman <alan.steadman@gmail.com>
2023-12-31 13:08:51 +0000

    Moved services configuration out of Program.cs and into extention methods in DependencyInjection.cs

---
+++ src/QuickClubs.AdminUI/QuickClubs.AdminUI/DependencyInjection.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.Extensions.Options;
+using QuickClubs.AdminUI.Authentication;
+using QuickClubs.AdminUI.Common.Listeners;
+using QuickClubs.AdminUI.Common.Models;
+using QuickClubs.AdminUI.Services;
+
+namespace QuickClubs.AdminUI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAdminUiClient(this IServiceCollection services)
+    {
+        services
+            .AddApiOptions()
+            .AddAuthentication()
+            .AddApiServices()
+            .AddNotFoundListener();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiOptions(this IServiceCollection services)
+    {
+        services.AddOptions<ApiSettings>()
+            .BindConfiguration(ApiSettings.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuthentication(this IServiceCollection services)
+    {
+        services
+            .AddScoped<ProtectedSessionStorage>()
+            .AddAuthenticationCore()
+            .AddTransient<AuthenticationHandler>()
+            .AddScoped<AuthenticationStateProvider, AppAuthenticationStateProvider>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IAuthenticationService, AuthenticationService>()
+            .AddScoped<ApiService>()
+            .AddScoped<IClubService, ClubService>();
+
+        services.AddHttpClient<IAuthenticationService, AuthenticationService>((serviceProvider, httpClient) =>
+            {
+                var apiSettings = serviceProvider.GetRequiredService<IOptions<ApiSettings>>().Value;
+
+                httpClient.DefaultRequestHeaders.Add("User-Agent", apiSettings.UserAgent);
+                httpClient.BaseAddress = new Uri(apiSettings.BaseUrl + "/auth");
+            });
+
+        services.AddHttpClient<ApiService>((serviceProvider, httpClient) =>
+            {
+                var apiSettings = serviceProvider.GetRequiredService<IOptions<ApiSettings>>().Value;
+
+                httpClient.DefaultRequestHeaders.Add("User-Agent", apiSettings.UserAgent);
+                httpClient.BaseAddress = new Uri(apiSettings.BaseUrl);
+            }).AddHttpMessageHandler<AuthenticationHandler>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddNotFoundListener(this IServiceCollection services)
+    {
+        services
+            .AddHttpContextAccessor() // needed for the NotFoundListener on MainLayout.razor
+            .AddScoped<NotFoundListener>();
+
+        return services;
+    }
+
+}
--- src/QuickClubs.AdminUI/QuickClubs.AdminUI/Program.cs
+++ src/QuickClubs.AdminUI/QuickClubs.AdminUI/Program.cs
@@ -1,56 +1,17 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
-using Microsoft.Extensions.Options;
-using QuickClubs.AdminUI.Authentication;
+using QuickClubs.AdminUI;
 using QuickClubs.AdminUI.Client.Pages;
-using QuickClubs.AdminUI.Common.Listeners;
-using QuickClubs.AdminUI.Common.Models;
 using QuickClubs.AdminUI.Components;
-using QuickClubs.AdminUI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddHttpContextAccessor(); // needed for the NotFoundListener on MainLayout.razor
-builder.Services.AddScoped<NotFoundListener>();
-
-builder.Services.AddScoped<ProtectedSessionStorage>();
-builder.Services.AddAuthenticationCore();
-builder.Services.AddTransient<AuthenticationHandler>();
-builder.Services.AddScoped<AuthenticationStateProvider, AppAuthenticationStateProvider>();
-
-builder.Services.AddOptions<ApiSettings>()
-    .BindConfiguration(ApiSettings.SectionName)
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-builder.Services.AddScoped<IClubService, ClubService>();
-
-builder.Services.AddHttpClient<IAuthenticationService, AuthenticationService>((serviceProvider, httpClient) =>
-{
-    var apiSettings = serviceProvider.GetRequiredService<IOptions<ApiSettings>>().Value;
-
-    httpClient.DefaultRequestHeaders.Add("User-Agent", apiSettings.UserAgent);
-    httpClient.BaseAddress = new Uri(apiSettings.BaseUrl + "/auth");
-});
-
-builder.Services.AddScoped<ApiService>();
-builder.Services.AddHttpClient<ApiService>((serviceProvider, httpClient) =>
-{
-    var apiSettings = serviceProvider.GetRequiredService<IOptions<ApiSettings>>().Value;
-
-    httpClient.DefaultRequestHeaders.Add("User-Agent", apiSettings.UserAgent);
-    httpClient.BaseAddress = new Uri(apiSettings.BaseUrl);
-});//.AddHttpMessageHandler<AuthenticationHandler>();
+builder.Services.AddAdminUiClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -58,7 +19,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
