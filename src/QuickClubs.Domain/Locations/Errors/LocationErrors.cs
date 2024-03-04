52177bfa3ea19a9121120c6daac21936d91bf593
Alan Steadman <alan.steadman@gmail.com>
2024-03-04 23:06:12 +0000

    LocationErrors

---
+++ src/QuickClubs.Domain/Locations/Errors/LocationErrors.cs
@@ -0,0 +1,9 @@
+﻿using QuickClubs.Domain.Abstractions;
+
+namespace QuickClubs.Domain.Locations.Errors;
+public static class LocationErrors
+{
+    public static Error NotFound(Guid locationId) => new("Location.NotFound", $"No location was found with the id '{locationId}'");
+
+    public static Error DuplicateName(string name) => new("Location.DuplicateFullName", $"A location already exists with the name '{name}' at this club");
+}
