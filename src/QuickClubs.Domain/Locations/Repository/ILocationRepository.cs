8b5b1ef8a2487bbffbba17bf96da402c0482f104
Alan Steadman <alan.steadman@gmail.com>
2024-03-04 23:06:37 +0000

    Location Repository

---
+++ src/QuickClubs.Domain/Locations/Repository/ILocationRepository.cs
@@ -0,0 +1,12 @@
+﻿using QuickClubs.Domain.Clubs.ValueObjects;
+using QuickClubs.Domain.Locations.ValueObjects;
+
+namespace QuickClubs.Domain.Locations.Repository;
+public interface ILocationRepository
+{
+    Task<Location?> GetByIdAsync(LocationId id, CancellationToken cancellationToken = default);
+    void Add(Location location);
+    void Update(Location location);
+
+    Task<bool> IsNameUniqueAsync(LocationId? id, ClubId clubId, string name, CancellationToken cancellationToken = default);
+}
